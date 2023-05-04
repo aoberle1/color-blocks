9f8fd2b95b33887287fe1304665aa0db2a1e9c13
Adam Oberle <oberle.adam@gmail.com>
2023-05-04 18:23:52 -0500

    whoops, was working with folks and completely forgot about commits, finished question 1 and question 2 in required homework

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+$(document).ready(OnReady);
+
+function OnReady() {
+    console.log( 'We are ready to rock!' );
+
+    $( '#red-btn' ).on( 'click', appendRedContainer );
+    $( '#blue-btn' ).on( 'click', appendBlueContainer);
+    $( '#green-btn' ).on( 'click', appendGreenContainer);
+    $( '#yellow-btn' ).on( 'click', appendYellowContainer);
+
+    $('#allBlocks').on('click', '.block', removeBlock);
+
+    
+}
+let redCount = 1
+let blueCount = 1
+let greenCount = 1
+let yellowCount = 1
+
+function appendRedContainer() {
+    $( '#allBlocks' ).append( '<div class="block red-fill"></div>' );
+    $('#red-count').empty()
+    redCount++
+    $('#red-count').append(redCount)
+}
+
+function appendBlueContainer() {
+    $( '#allBlocks' ).append( '<div class="block blue-fill"></div>' );
+    $('#blue-count').empty()
+    blueCount++
+    $('#blue-count').append(blueCount)
+}
+
+function appendGreenContainer( ) {
+    $( '#allBlocks' ).append( '<div class="block green-fill"></div>' );
+}
+
+function appendYellowContainer() {
+    $( '#allBlocks' ).append( '<div class="block yellow-fill"></div>' );
+}
+
+function removeBlock() {
+    $( this ).remove();
+}
+
+
+
+
+
 console.log('🟥 🟦 🟩 🟨');
